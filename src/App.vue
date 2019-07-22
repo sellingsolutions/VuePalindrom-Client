51304d6f3a7688bc4f4d5e9032bd306387cc1ab2
Konstantin <const.miyang@gmail.com>
2019-07-22 21:05:05 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,75 @@
 <template>
-  <HelloWorld v-model="obj" />
-  <!-- v-model directive has special meaning in Vue. It creates 2-way data binding. See: https://vuejs.org/v2/guide/components.html#Using-v-model-on-Components -->
+    <HelloWorld v-model="obj" />
+    <!-- v-model directive has special meaning in Vue. It creates 2-way data binding. See: https://vuejs.org/v2/guide/components.html#Using-v-model-on-Components -->
 </template>
 
 <script>
-// the below line is only needed to simulate the server
-import '../mock-server/dist/mock-server.js'
-import { PalindromDOM } from 'palindrom'
-import HelloWorld from './components/HelloWorld.vue'
+    // the below line is only needed to simulate the server
+    // import '../mock-server/dist/mock-server.js'
+    import { PalindromDOM } from 'palindrom'
+    import HelloWorld from './components/HelloWorld.vue'
+    import Http from 'http';
 
-function connectToPalindrom(onConnect) { // this is a generic Palindrom client config. See https://palindrom.github.io/docs/04-_PalindromDOM/
-  new PalindromDOM({
-    // "remoteUrl" is the location of the Palindrom server. By convention, the server should respond with a Palindrom
-    // session JSON object when request has a header "Accept: application/json". If the Palindrom server is located in
-    // another location, provide the URL here. Make sure that this URL is always the same, however every request to
-    // it creates a new Palindrom session, which individual URL is given in the "X-Location" header of the response
-    // to "remoteUrl"
-    remoteUrl: window.location.href,
-    onStateReset: onConnect,
-    debug: false,
-    localVersionPath: "/_ver#c$",
-    remoteVersionPath: "/_ver#s",
-    ot: true,
-    useWebSocket: false // with not simulated server, change this to true
-  });
-}
+    function requestPalindromConnection(onConnect) {
+        Http.get("http://localhost:8080/VuePalindromServer/NewConnection", (response) => {
+            let data = "";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  methods: {
-    onConnect(obj) {
-      // use Palindrom's data object in the "App" component's data
-      this.obj = obj;
+            // A chunk of data has been recieved.
+            response.on("data", (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            response.on("end", () => {
+                console.log("NewConnection: ", data);
+
+                connectToPalindrom(data, onConnect);
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
     }
-  },
-  created() {
-    // when an instance of the "App" component is created, request Palindrom connection and provide a callback
-    connectToPalindrom(this.onConnect);
-  },
-  data: function() {
-    return {
-      obj: null
-    };
-  }
-}
+
+    function connectToPalindrom(remoteUrl, onConnect) {
+        // this is a generic Palindrom client config. See https://palindrom.github.io/docs/04-_PalindromDOM/
+        new PalindromDOM({
+            // "remoteUrl" is the location of the Palindrom server. By convention, the server should respond with a Palindrom
+            // session JSON object when request has a header "Accept: application/json". If the Palindrom server is located in
+            // another location, provide the URL here. Make sure that this URL is always the same, however every request to
+            // it creates a new Palindrom session, which individual URL is given in the "X-Location" header of the response
+            // to "remoteUrl"
+            //remoteUrl: window.location.href,
+            remoteUrl: remoteUrl,
+            onStateReset: onConnect,
+            debug: false,
+            localVersionPath: "/_ver#c$",
+            remoteVersionPath: "/_ver#s",
+            ot: true,
+            useWebSocket: true // with not simulated server, change this to true
+        });
+    }
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        },
+        methods: {
+            onConnect(obj) {
+                // use Palindrom's data object in the "App" component's data
+                console.log("Connected to Palindrom", obj, JSON.stringify(obj));
+                this.obj = obj;
+            }
+        },
+        created() {
+            // when an instance of the "App" component is created, request Palindrom connection and provide a callback
+            requestPalindromConnection(this.onConnect);
+        },
+        data: function () {
+            return {
+                obj: null
+            };
+        }
+    }
 </script>
